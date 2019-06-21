b6b42308fb9994e09311908438747c40a6466b09
BogacSabuncu <bsabuncu@mac.com>
2019-06-21 13:47:45 -0400

    added testing for /api/signup

--- test/api-test.js
+++ test/api-test.js
@@ -20,32 +20,181 @@ describe("GET /api/users", function () {
 
     it("should return all the users", function (done) {
         db.User.insertMany(usrArr)
-        .then(function (docs) {
-            request.get("/api/users").end(function (err, res) {
-                let responseStatus = res.status;
-                let responseBody = res.body;
+            .then(function (docs) {
+                request.get("/api/users").end(function (err, res) {
+                    let responseStatus = res.status;
+                    let responseBody = res.body;
+
+                    expect(err).to.be.null;
+
+                    expect(responseStatus).to.equal(200);
+
+                    expect(responseBody)
+                        .to.be.an("array")
+                        .that.has.lengthOf(2);
+
+                    expect(responseBody[0])
+                        .to.be.an("object")
+                        .that.includes({ username: "User1", password: "password1" });
+
+                    expect(responseBody[1])
+                        .to.be.an("object")
+                        .that.includes({ username: "User2", password: "password2" });
+
+                    done();
+                });
+            }).catch(err => {
+                console.log(err);
+                done();
+            });
+    });
+});
+
+describe("POST /api/signup", function () {
+    beforeEach(function (done) {
+        request = chai.request(server);
+        db.User.deleteMany({}).then(function () {
+            done();
+        });
+    });
+
+    it("should create a new user with the fields", function (done) {
+        let reqBody = {
+            username: "createdUser",
+            password: "createdPassword",
+            gender: "bot",
+            age: "28"
+        }
+
+        request
+            .post("/api/signup")
+            .send(reqBody)
+            .end(function (err, res) {
+                var responseStatus = res.status;
+                var responseBody = res.body;
+
+                // Run assertions on the response
 
                 expect(err).to.be.null;
 
                 expect(responseStatus).to.equal(200);
 
                 expect(responseBody)
-                    .to.be.an("array")
-                    .that.has.lengthOf(2);
-
-                expect(responseBody[0])
                     .to.be.an("object")
-                    .that.includes({ username: "User1", password: "password1" });
+                    .that.includes({ message: "user created" });
 
-                expect(responseBody[1])
+                // The `done` function is used to end any asynchronous tests
+                db.User.findOne({
+                    username: reqBody.username
+                }).then(function (dbUser) {
+                    expect(dbUser).to.not.be.null;
+
+                    done();
+                });
+            });
+    });
+
+    it("should return an error when usename is not given", function (done) {
+        let reqBody = {
+            username: "",
+            password: "noUsername",
+            gender: "bot",
+            age: "28"
+        }
+
+        request
+            .post("/api/signup")
+            .send(reqBody)
+            .end(function (err, res) {
+                var responseStatus = res.status;
+                var responseBody = res.body;
+
+                // Run assertions on the response
+
+                expect(responseStatus).to.equal(500);
+                expect(responseBody)
                     .to.be.an("object")
-                    .that.includes({ username: "User2", password: "password2" });
+                    .that.includes({ error: "User validation failed: username: Path `username` is required." });
 
+
+                // The `done` function is used to end any asynchronous tests
                 done();
             });
-        }).catch(err => {
-            console.log(err);
-            done();
-        });
     });
+
+    it("should return an error when password is not given", function (done) {
+        let reqBody = {
+            username: "noPasswords",
+            password: "",
+            gender: "bot",
+            age: "28"
+        }
+
+        request
+            .post("/api/signup")
+            .send(reqBody)
+            .end(function (err, res) {
+                var responseStatus = res.status;
+                var responseBody = res.body;
+
+                // Run assertions on the response
+
+                expect(responseStatus).to.equal(500);
+                expect(responseBody)
+                    .to.be.an("object")
+                    .that.includes({ error: "User validation failed: password: Path `password` is required." });
+
+
+                // The `done` function is used to end any asynchronous tests
+                done();
+            });
+    });
+
 });
+
+// describe("POST /api/authenticate", function () {
+//     beforeEach(function (done) {
+//         request = chai.request(server);
+//         db.User.deleteMany({}).then(function () {
+//             let reqBody = {
+//                 username: "createdUser",
+//                 password: "createdPassword",
+//                 gender: "bot",
+//                 age: "28"
+//             };
+
+//             db.User.create(reqBody).then(function () {
+//                 done();
+//             });
+//         });
+//     });
+
+//     it("should return an error wrong usename/password entered is not given", function (done) {
+//         let reqBody = {
+//             username: "",
+//             password: "noUsername",
+//             gender: "bot",
+//             age: "28"
+//         }
+
+//         request
+//             .post("/api/signup")
+//             .send(reqBody)
+//             .end(function (err, res) {
+//                 var responseStatus = res.status;
+//                 var responseBody = res.body;
+
+//                 // Run assertions on the response
+
+//                 expect(responseStatus).to.equal(500);
+//                 expect(responseBody)
+//                     .to.be.an("object")
+//                     .that.includes({ error: "User validation failed: username: Path `username` is required." });
+
+
+//                 // The `done` function is used to end any asynchronous tests
+//                 done();
+//             });
+//     });
+
+// })
