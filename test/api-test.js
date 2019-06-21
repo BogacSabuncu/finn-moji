42d903423fc9522dd8658d025c9755a4a5f0e976
BogacSabuncu <bsabuncu@mac.com>
2019-06-21 14:22:42 -0400

    test added for /api/authenticate

--- test/api-test.js
+++ test/api-test.js
@@ -152,49 +152,77 @@ describe("POST /api/signup", function () {
 
 });
 
-// describe("POST /api/authenticate", function () {
-//     beforeEach(function (done) {
-//         request = chai.request(server);
-//         db.User.deleteMany({}).then(function () {
-//             let reqBody = {
-//                 username: "createdUser",
-//                 password: "createdPassword",
-//                 gender: "bot",
-//                 age: "28"
-//             };
+describe("POST /api/authenticate", function () {
+    beforeEach(function (done) {
+        request = chai.request(server);
+        db.User.deleteMany({}).then(function () {
+            let testUser = {
+                username: "createdUser",
+                password: "createdPassword",
+                gender: "bot",
+                age: "28"
+            };
 
-//             db.User.create(reqBody).then(function () {
-//                 done();
-//             });
-//         });
-//     });
+            db.User.create(testUser).then(function () {
+                done();
+            });
+        });
+    });
 
-//     it("should return an error wrong usename/password entered is not given", function (done) {
-//         let reqBody = {
-//             username: "",
-//             password: "noUsername",
-//             gender: "bot",
-//             age: "28"
-//         }
+    it("should return an error wrong usename/password entered", function (done) {
+        let reqBody = {
+            username: "createdUser",
+            password: "WRONGPASSWORD",
+            gender: "bot",
+            age: "28"
+        }
 
-//         request
-//             .post("/api/signup")
-//             .send(reqBody)
-//             .end(function (err, res) {
-//                 var responseStatus = res.status;
-//                 var responseBody = res.body;
+        request
+            .post("/api/authenticate")
+            .send(reqBody)
+            .end(function (err, res) {
+                var responseStatus = res.status;
+                var responseBody = res.body;
 
-//                 // Run assertions on the response
+                // Run assertions on the response
 
-//                 expect(responseStatus).to.equal(500);
-//                 expect(responseBody)
-//                     .to.be.an("object")
-//                     .that.includes({ error: "User validation failed: username: Path `username` is required." });
+                expect(responseStatus).to.equal(401);
+                expect(responseBody)
+                    .to.be.an("object")
+                    .that.includes({ message: "Username or password is incorrect." });
 
 
-//                 // The `done` function is used to end any asynchronous tests
-//                 done();
-//             });
-//     });
+                // The `done` function is used to end any asynchronous tests
+                done();
+            });
+    });
 
-// })
+    it("should return a token when username/password entered correctly", function (done) {
+        let reqBody = {
+            username: "createdUser",
+            password: "createdPassword",
+            gender: "bot",
+            age: "28"
+        }
+
+        request.post("/api/authenticate")
+            .send(reqBody)
+            .end(function (err, res) {
+                var responseStatus = res.status;
+                var responseBody = res.body;
+                
+                expect(err).to.be.null;
+
+                expect(responseStatus).to.equal(200);
+
+                expect(responseBody)
+                    .to.be.an("object")
+                    .that.includes({ username: reqBody.username });
+
+                expect(responseBody.token).to.not.be.null;
+
+
+                done()
+            });
+    });
+});
