21f61cb20cd9193de84832e78f8263fd9a10db54
fyeh0 <fangiyeh@gmail.com>
2019-06-24 14:11:07 -0400

    signup style, added button to landing page

--- client/src/components/SingupForm.js
+++ client/src/components/SingupForm.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { withRouter } from "react-router-dom";
 import UserContext from "../context/UserContext";
 // import Auth from "../utils/Auth";
 import API from "../utils/API";
 import { Container, Row, Col } from "react-bootstrap";
-import { MDBBtn } from "mdbreact";
+import { MDBInput, MDBBtn } from "mdbreact";
 
 class SignupForm extends Component {
   static contextType = UserContext;
@@ -37,10 +37,12 @@ class SignupForm extends Component {
   render() {
     return (
       <Container>
-        <form onSubmit={this.submitHandler}>
+        <form className="needs-validation" onSubmit={this.submitHandler} >
           <Row>
             <Col>
-              <input
+              <MDBInput
+                icon="user"
+                label="Username"
                 type="text"
                 name="username"
                 value={this.state.username}
@@ -49,7 +51,9 @@ class SignupForm extends Component {
               />
             </Col>
             <Col>
-              <input
+              <MDBInput
+                icon="lock"
+                label="Password"
                 type="password"
                 name="password"
                 value={this.state.password}
@@ -58,45 +62,80 @@ class SignupForm extends Component {
               />
             </Col>
           </Row>
-          <input
-            type="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.changeHandler}
-            placeholder="Email"
-          />
-          <input
-            type="text"
-            name="gender"
-            value={this.state.gender}
-            onChange={this.changeHandler}
-            placeholder="Gender"
-          />
-          <input
-            type="text"
-            name="age"
-            value={this.state.age}
-            onChange={this.changeHandler}
-            placeholder="Age"
-          />
-          <input
-            type="text"
-            name="income"
-            value={this.state.income}
-            onChange={this.changeHandler}
-            placeholder="Income"
-          />
-          <input
-            type="expenses"
-            name="expenses"
-            value={this.state.expenses}
-            onChange={this.changeHandler}
-            placeholder="Expenses"
-          />
-
-          <button type="submit" onClick={this.submitHandler}>
-            Create
-          </button>
+          <Row>
+            <Col>
+              <MDBInput
+                icon="envelope"
+                label="Email"
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.changeHandler}
+                placeholder="Email"
+              />
+            </Col>
+            <Col>
+              <MDBInput
+                icon="restroom"
+                label="Gender"
+                type="text"
+                name="gender"
+                value={this.state.gender}
+                onChange={this.changeHandler}
+                placeholder="Gender"
+              />
+            </Col>
+            <Col>
+              <MDBInput
+                icon="sort-numeric-up"
+                label="Age"
+                type="text"
+                name="age"
+                value={this.state.age}
+                onChange={this.changeHandler}
+                placeholder="Age"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <MDBInput
+                icon="dollar-sign"
+                label="Monthly Income"
+                type="text"
+                name="income"
+                value={this.state.income}
+                onChange={this.changeHandler}
+                placeholder="Income"
+              />
+            </Col>
+            <Col>
+              <MDBInput
+                icon="dollar-sign"
+                label="Monthly Expenses"
+                type="text"
+                name="expenses"
+                value={this.state.expenses}
+                onChange={this.changeHandler}
+                placeholder="Expenses"
+              />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="text-center">
+                <MDBBtn
+                  href="/profile"
+                  size="lg"
+                  gradient="blue"
+                  // type="submit"
+                  onClick={this.submitHandler}
+                >
+                  Create Your Account!
+                </MDBBtn>
+              </div>
+            </Col>
+          </Row>
         </form>
       </Container>
     );
