54a51ce47379090d9942510a28b1893fe8eac192
fyeh0 <fangiyeh@gmail.com>
2019-06-24 11:24:21 -0400

    import react bootstrap for container, row, col setup

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import { withRouter } from "react-router-dom";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "../stylesheets/Landing.css";
 import { MDBBtn } from "mdbreact";
 
--- client/src/components/SingupForm.js
+++ client/src/components/SingupForm.js
@@ -3,6 +3,8 @@ import { withRouter } from "react-router-dom";
 import UserContext from "../context/UserContext";
 // import Auth from "../utils/Auth";
 import API from "../utils/API";
+import { Container, Row, Col } from "react-bootstrap";
+import { MDBBtn } from "mdbreact";
 
 class SignupForm extends Component {
   static contextType = UserContext;
@@ -34,61 +36,69 @@ class SignupForm extends Component {
 
   render() {
     return (
-      <form onSubmit={this.submitHandler}>
-        <input
-          type='text'
-          name='username'
-          value={this.state.username}
-          onChange={this.changeHandler}
-          placeholder='Username'
-        />
-        <input
-          type='password'
-          name='password'
-          value={this.state.password}
-          onChange={this.changeHandler}
-          placeholder='Password'
-        />
-        <input
-          type='email'
-          name='email'
-          value={this.state.email}
-          onChange={this.changeHandler}
-          placeholder='Email'
-        />
-        <input
-          type='text'
-          name='gender'
-          value={this.state.gender}
-          onChange={this.changeHandler}
-          placeholder='Gender'
-        />
-        <input
-          type='text'
-          name='age'
-          value={this.state.age}
-          onChange={this.changeHandler}
-          placeholder='Age'
-        />
-        <input
-          type='text'
-          name='income'
-          value={this.state.income}
-          onChange={this.changeHandler}
-          placeholder='Income'
-        />
-        <input
-          type='expenses'
-          name='expenses'
-          value={this.state.expenses}
-          onChange={this.changeHandler}
-          placeholder='Expenses'
-        />
+      <Container>
+        <form onSubmit={this.submitHandler}>
+          <Row>
+            <Col>
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.changeHandler}
+                placeholder="Username"
+              />
+            </Col>
+            <Col>
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.changeHandler}
+                placeholder="Password"
+              />
+            </Col>
+          </Row>
+          <input
+            type="email"
+            name="email"
+            value={this.state.email}
+            onChange={this.changeHandler}
+            placeholder="Email"
+          />
+          <input
+            type="text"
+            name="gender"
+            value={this.state.gender}
+            onChange={this.changeHandler}
+            placeholder="Gender"
+          />
+          <input
+            type="text"
+            name="age"
+            value={this.state.age}
+            onChange={this.changeHandler}
+            placeholder="Age"
+          />
+          <input
+            type="text"
+            name="income"
+            value={this.state.income}
+            onChange={this.changeHandler}
+            placeholder="Income"
+          />
+          <input
+            type="expenses"
+            name="expenses"
+            value={this.state.expenses}
+            onChange={this.changeHandler}
+            placeholder="Expenses"
+          />
 
-        <button type='submit' onClick={this.submitHandler}>
-          Create
-        </button>
-      </form>
+          <button type="submit" onClick={this.submitHandler}>
+            Create
+          </button>
+        </form>
+      </Container>
     );
   }
 }
