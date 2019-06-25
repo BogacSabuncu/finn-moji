3223141dda2a884bdad0fd6d21536ae40f96144f
BogacSabuncu <bsabuncu@mac.com>
2019-06-25 15:15:39 -0400

    adding donut card

---
+++ client/src/components/DonutCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { MDBContainer, MDBCardHeader, MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
+import DonutChart from "./Donut.js";
+
+const StatsCard = () => {
+    return (
+    <MDBContainer>
+      <MDBCard style={{ width: "22rem", marginTop: "1rem" }}>
+        <MDBCardHeader color="deep-orange lighten-1">Your Statistics:</MDBCardHeader>
+        <MDBCardBody>
+          <MDBCardTitle></MDBCardTitle>
+          <MDBCardText>
+          <Charts />
+          </MDBCardText>
+          
+        </MDBCardBody>
+      </MDBCard>
+    </MDBContainer>
+    );
+    };
+    
+    export default StatsCard;
